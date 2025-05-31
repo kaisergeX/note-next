0418cb4386087899d7704a7fe76e5820dff742ee
kaisergeX <khaivd98@gmail.com>
2025-05-31 18:26:00 +0700

    feat: enable static rendering for dynamic `[locale]` route

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,16 @@
 import {Analytics} from '@vercel/analytics/react'
 import {SpeedInsights} from '@vercel/speed-insights/next'
 import type {Metadata} from 'next'
-import {useLocale, useTranslations} from 'next-intl'
-import {getTranslations} from 'next-intl/server'
+import {hasLocale} from 'next-intl'
+import {getTranslations, setRequestLocale} from 'next-intl/server'
 import {Inter} from 'next/font/google'
 import {notFound} from 'next/navigation'
-import {type PropsWithChildren, use} from 'react'
+import {type PropsWithChildren} from 'react'
 import Navbar from '~/components/layouts/navbar'
 import ProviderWrapper from '~/components/layouts/provider-wrapper'
 import ScrollTopButton from '~/components/layouts/scroll-top-button'
 import ThemeWrapper from '~/components/layouts/theme-wrapper'
+import {localeRouting} from '~/config/localization'
 import type {PropsWithLocale} from '~/types'
 import '../globals.css'
 
@@ -25,20 +26,27 @@ export async function generateMetadata(
   }
 }
 
+export function generateStaticParams() {
+  return localeRouting.locales.map((locale) => ({locale}))
+}
+
 const inter = Inter({subsets: ['latin'], variable: '--font-inter'})
 
-export default function LocaleLayout({
+export default async function LocaleLayout({
   children,
   params,
 }: PropsWithChildren<PropsWithLocale>) {
-  const localeParams = use(params).locale
-  const locale = useLocale()
-  const t = useTranslations()
+  const locale = (await params).locale
 
   // Show a 404 error if the user requests an unknown locale
-  if (localeParams !== locale) {
+  if (!hasLocale(localeRouting.locales, locale)) {
     notFound()
   }
+
+  // Enable static rendering
+  setRequestLocale(locale)
+
+  const t = await getTranslations({locale})
 
   return (
     <ThemeWrapper lang={locale} className={inter.variable}>
